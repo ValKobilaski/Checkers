f068a872a245673d1fc4fb68ba086fee8bf67998
ValKobilaski <kobilaski@gmail.com>
2021-02-06 21:09:11 -0500

    initial commit, added game clock

---
+++ checkers/constants.py
@@ -0,0 +1,17 @@
+import pygame
+
+#Display shape
+WIDTH = 800
+HEIGHT = 800
+
+# Board shape
+ROWS = 8
+COLS = 8
+SQUARE_SIZE = WIDTH // COLS
+
+# RGB colour codes 
+RED =  (255, 0, 0)
+GREEN = (255, 0, 0)
+BLUE = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0) 
