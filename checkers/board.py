7bb76493af27fb562a187ad0efb0591ce66aa98f
ValKobilaski <kobilaski@gmail.com>
2021-02-07 23:36:28 -0500

    Added pieces and draw method

--- checkers/board.py
+++ checkers/board.py
@@ -1,5 +1,6 @@
 import pygame
-from .constants import BOARD_BEIGE, BOARD_BROWN, ROWS, SQUARE_SIZE
+from .constants import BOARD_BEIGE, BOARD_BROWN, ROWS, COLS, SQUARE_SIZE, WHITE, BLACK
+from .piece import Piece
 
 
 class Board:
@@ -10,6 +11,7 @@ class Board:
         self.blackLeft = 12
         self.whiteKings = 0
         self.blackKings = 0
+        self.create_board()
 
     def draw_squares(self, win):
         win.fill(BOARD_BROWN)
@@ -21,3 +23,26 @@ class Board:
                     BOARD_BEIGE,
                     (row * SQUARE_SIZE, col * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE),
                 )
+
+    def create_board(self):
+        for row in range(ROWS):
+            self.board.append([])
+            for col in range(COLS):
+                if col % 2 == ((row + 1) % 2):
+
+                    if row < 3:
+                        self.board[row].append(Piece(row, col, WHITE))
+                    elif row > 4:
+                        self.board[row].append(Piece(row, col, BLACK))
+                    else:
+                        self.board[row].append(0)
+                else:
+                    self.board[row].append(0)
+
+    def draw(self, win):
+        self.draw_squares(win)
+        for row in range(ROWS):
+            for col in range(COLS):
+                piece = self.board[row][col]
+                if piece != 0:
+                    piece.draw(win)
---
+++ checkers/piece.py
@@ -0,0 +1,38 @@
+import pygame
+from .constants import WHITE, BLACK, GREY, SQUARE_SIZE
+
+
+class Piece:
+
+    PADDING = 10
+    OUTLINE = 2
+
+    def __init__(self, row, col, colour):
+        self.row = row
+        self.col = col
+        self.colour = colour
+        self.king = False
+
+        if self.colour == WHITE:
+            self.direction = 1
+        else:
+            self.direction = -1
+
+        self.x = 0
+        self.y = 0
+        self.calc_pos()
+
+    def calc_pos(self):
+        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
+        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
+
+    def make_king(self):
+        self.king = True
+
+    def draw(self, win):
+        radius = SQUARE_SIZE // 2 - self.PADDING
+        pygame.draw.circle(win, GREY, (self.x, self.y), radius + self.OUTLINE)
+        pygame.draw.circle(win, self.colour, (self.x, self.y), radius)
+
+    def __repr__(self):
+        return str(self.colour)
